ad40a4908c71e1914575185fb226fdd4ff0e2555
Santiago Galindo <sp.galindoh@gmail.com>
2015-12-07 08:47:15 -0500

    Español

--- client/helpers/autoform-hooks.js
+++ client/helpers/autoform-hooks.js
@@ -3,7 +3,8 @@ let hooksObject = {
     Bert.alert('La información ha sido guardada exitosamente', 'success');
   },
   onError: function (formType, error) {
-    Bert.alert( `No se guardó la información, revise sus datos. Error: ${error.reason}`, 'warning' );
+    console.log(error);
+    Bert.alert( `No se guardó la información, revise sus datos. ${error}`, 'warning' );
   }
 };
 

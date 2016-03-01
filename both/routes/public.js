3b1d8dfa72b3269f4ed652ad5866d998fda36971
Santiago Galindo <sp.galindoh@gmail.com>
2016-03-01 08:21:02 -0500

    Public pages centered

--- both/routes/public.js
+++ both/routes/public.js
@@ -12,34 +12,34 @@ const publicRoutes = FlowRouter.group({
 
 publicRoutes.route('/', {
   action() {
-    BlazeLayout.render('default', {yield: 'login'});
+    BlazeLayout.render('defaultPublic', {yield: 'login'});
   }
 });
 
 publicRoutes.route('/login', {
   name: 'login',
   action() {
-    BlazeLayout.render('default', {yield: 'login'});
+    BlazeLayout.render('defaultPublic', {yield: 'login'});
   }
 });
 
 publicRoutes.route('/invite/:token', {
   name: 'invite',
   action() {
-    BlazeLayout.render('default', {yield: 'invite'});
+    BlazeLayout.render('defaultPublic', {yield: 'invite'});
   }
 });
 
 publicRoutes.route('/recover-password', {
   name: 'recover-password',
   action() {
-    BlazeLayout.render('default', {yield: 'recoverPassword'});
+    BlazeLayout.render('defaultPublic', {yield: 'recoverPassword'});
   }
 });
 
 publicRoutes.route('/reset-password/:token', {
   name: 'reset-password',
   action() {
-    BlazeLayout.render('default', {yield: 'resetPassword'});
+    BlazeLayout.render('defaultPublic', {yield: 'resetPassword'});
   }
 });
